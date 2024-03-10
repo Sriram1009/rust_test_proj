1762bf97b0140963a6a7f451522c4cd4631ca791
Sriram1009 <sriram1009@gmail.com>
2024-03-10 21:26:26 +0530

    tried the pointer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// use std::io
+
 fn main() {
     println!("Hello, world!");
+
+    let mut data = 110;
+
+    println!("{}", test_function(&mut data))
+
+}
+
+fn test_function(data: &mut i32 ) -> i32{
+    *data = *data +1;
+    *data + 10
 }
